9204354d92ea9c0dcda232a20eb36b8fffb80417
Binh Nguyen <nguyenvulebinh@gmail.com>
2022-11-14 18:07:31 +0100

    add infer sample

--- voice_filter_infer.py
+++ voice_filter_infer.py
@@ -1,7 +1,7 @@
-# from src.spnn.models.asr_voice_filter.modeling_enh_asr import ASRVoiceFilter
 from src.model.modeling_enh_asr import ASRVoiceFilter
 import torch
 from src.net.xvector_sincnet import XVectorSincNet
+from huggingface_hub import hf_hub_download
 import os
 import glob
 import csv
@@ -10,14 +10,13 @@ import librosa
 import numpy as np
 import soundfile as sf
 
-# os.environ['CUDA_VISIBLE_DEVICES'] = ""
-os.environ['OMP_NUM_THREADS'] = "1"
-os.environ['MKL_NUM_THREADS'] = "1"
+
 use_gpu = False
 if use_gpu:
     if not torch.cuda.is_available():
         use_gpu = False
-
+        
+        
 def load_xvector_sincnet_model(model_file):
     meta = torch.load(model_file, map_location='cpu')['state_dict']
     xvector_model = XVectorSincNet()
@@ -37,28 +36,28 @@ def cal_xvector_sincnet_embedding(xvector_model, ref_wav, max_length=5, sr=16000
     embed = xvector_model(wavs.unsqueeze(1).float())
     return torch.mean(embed, dim=0).detach().cpu()
 
-if __name__ == "__main__":    
-    pretrained_path = './model-bin/voice_enhancing'
-    asr_enh_model = ASRVoiceFilter.from_pretrained(pretrained_path)
-    xvector_model = load_xvector_sincnet_model('./model-bin/speaker_embedding/xvector_sincnet.pt')
-    
-    if use_gpu:
-        asr_enh_model = asr_enh_model.cuda()
-        xvector_model = xvector_model.cuda()
 
-    print(xvector_model)
-    print(asr_enh_model)
-    
-    mix_wav_path = "binh_and_linh_and_music.wav"
-    ref_wav_path = "binh_ref_long.wav"
-    output_wav_path = "binh_and_linh_and_music-binh_output_big.wav"
+if __name__ == "__main__":
+    # Load models
+    repo_id = 'nguyenvulebinh/voice-filter'
+    enh_model = ASRVoiceFilter.from_pretrained(repo_id, cache_dir='./cache')
+    xvector_sincnet_model_path = hf_hub_download(repo_id=repo_id, filename="xvector_sincnet.pt", cache_dir='./cache')
+    xvector_model = load_xvector_sincnet_model(xvector_sincnet_model_path)
+    if use_gpu:
+        enh_model = enh_model.cuda()
+        xvector_model = xvector_model.cuda()
+        
+    # Load some audio sample
+    mix_wav_path = hf_hub_download(repo_id=repo_id, filename="binh_linh_newspaper_music_noise.wav", cache_dir='./cache')
+    # ref_wav_path = hf_hub_download(repo_id=repo_id, filename="binh_ref_long.wav", cache_dir='./cache')
+    ref_wav_path = hf_hub_download(repo_id=repo_id, filename="linh_ref_long.wav", cache_dir='./cache')
+    output_wav_path = "output.wav"
     mixed_wav, _ = librosa.load(mix_wav_path, sr=16000)
     ref_wav, _ = librosa.load(ref_wav_path, sr=16000)
-
-    # Calculate target speaker embedding
     
+    
+    # Calculate target speaker embedding
     xvector = cal_xvector_sincnet_embedding(xvector_model, ref_wav)
-            
     # Speech enhancing
     max_amp = np.abs(mixed_wav).max()
     mix_scaling = 1 / max_amp
@@ -67,12 +66,10 @@ if __name__ == "__main__":
     if use_gpu:
         mixed_wav_tf = mixed_wav_tf.cuda()
         xvector= xvector.cuda()
-    est_wav = asr_enh_model.do_enh(mixed_wav_tf, xvector).cpu().detach().numpy()
-
-    # write output file
+    est_wav = enh_model.do_enh(mixed_wav_tf, xvector).cpu().detach().numpy()
+    # Normalize estimated wav
     max_amp = np.abs(est_wav).max()
     mix_scaling = 1 / max_amp
     est_wav = mix_scaling * est_wav
-    
-    # est_wav = mixed_wav - est_wav
+    # write output file
     sf.write(output_wav_path, est_wav, 16000)
